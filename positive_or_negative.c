7137a09b825461982ea818e48969ea215a0cf8dd
NSHUTI Henry <henrixoson@gmail.com>
2023-03-23 13:01:27 -0700

    The debugging

---
+++ positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * positive_or_negative - whatever
+ * @i: whatever
+ */
+void positive_or_negative(int i)
+{
+if (i > 0)
+	{
+	printf("%d is positive\n", i);
+	}
+if (i < 0)
+	{
+	printf("%d is negative\n", i);
+	}
+if (i == 0)
+	{
+	printf("%d is zero\n", i);
+	}
+}
