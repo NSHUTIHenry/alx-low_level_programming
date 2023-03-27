8c5bfecff67d2017669ad49acd7b5c3fd7b53342
NSHUTI Henry <henrixoson@gmail.com>
2023-03-27 13:34:45 -0700

    The pointers 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_rev - only go one way
+* @s: print
+*/
+void print_rev(char *s)
+{
+int len = 0;
+
+while (*s != '\0')
+{
+len++;
+s++;
+}
+s--
+while (len > 0)
+{
+s--;
+len--;
+}
+}
