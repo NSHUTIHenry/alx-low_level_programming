3f6e977bbc02e9a7a262fdd2ef8da82a9a736c2a
NSHUTI Henry <henrixoson@gmail.com>
2023-03-27 14:48:14 -0700

    The pointers 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * rev_string - function to reverses a string
+ * @s: reverse
+ * Return: length
+ */
+void rev_string(char *s)
+{
+int len = 0;
+
+while (*s != '\0')
+{
+s--;
+len--;
+}
+return (len)
+}
