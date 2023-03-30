07bc198f50269d5ed4e4824d679cff5b0d9b12a0
NSHUTI Henry <henrixoson@gmail.com>
2023-03-30 14:30:11 -0700

    The last

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *leet - encode string
+ * @a: - string
+ * Return: letters
+ */
+char *leet(char *a)
+{
+int i;
+int j;
+
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
+
+for (i - 0; a[i] !-'\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (a[i] == s1[j])
+{
+a[i] = s2[j];
+}
+}
+return (a);
+}
+
