2908521f0a8c4176d6d441d2d9d0c77b071190b1
NSHUTI Henry <henrixoson@gmail.com>
2023-04-03 12:48:06 -0700

    The even more pointers 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - function that copies memory with area.
+ * @dest: Memory area to be copied to
+ * @src: Memory area to be copied from
+ * @n: amount of bytes to be copied
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	int i;
+	int j = n;
+
+	for (i = 0; i < j; i++)
+	{
+	dest[i] = src[i];
+		n--;
+	}
+	return (dest);
+}
