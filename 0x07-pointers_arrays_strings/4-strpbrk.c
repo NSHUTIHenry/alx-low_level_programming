98a03ae2911fb0fd34904975ab0a394a7777fb90
NSHUTI Henry <henrixoson@gmail.com>
2023-04-03 14:34:47 -0700

    The even more pointers 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,11 +16,10 @@ char *_strpbrk(char *s, char *accept)
 		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
-			{
-				return (s);
-			}
+			return (s);
+		}
 			s++;
 		}
 		return ('\0');
-	}
+
 }
