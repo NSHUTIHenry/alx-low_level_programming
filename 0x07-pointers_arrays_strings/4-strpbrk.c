a79905a6b6191121274a26da30c6908f77c900bd
NSHUTI Henry <henrixoson@gmail.com>
2023-04-03 14:36:33 -0700

    The even more pointers 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
  * *_strpbrk - function that searches a string for any of a set of bytes
  * @s: character
  * @accept: matches the bytes
- * Return: s
+ * Return: ('\0')
  */
 
 char *_strpbrk(char *s, char *accept)
