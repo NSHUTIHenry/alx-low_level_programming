08b5ecf3b8f66c57250bba8424fd4dc1fc2be858
NSHUTI Henry <henrixoson@gmail.com>
2023-04-03 12:09:14 -0700

    The even more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with bytes
+ * @s: Memory pointer
+ * @b: The constant byte
+ * @n: First n bytes
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (i = 0; n > 0; i++)
+	{
+		s(i) = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+READM.md even more pointers
