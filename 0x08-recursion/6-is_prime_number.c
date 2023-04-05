66388cfa88924ff4ad50d4d4bb3367646d518543
NSHUTI Henry <henrixoson@gmail.com>
2023-04-05 15:19:06 -0700

    The recursion 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _helper - The helper that print prime number
+ * @n: character
+ * @s: character
+ * Return: n
+ */
+
+int _helper(int n, int s)
+{
+	if (s == 1)
+	{
+		return (1);
+	}
+	if (n % s && s > 0)
+	{
+		return (0);
+	}
+	else
+		return (_helper(n, s - 1));
+}
+
+/**
+ * is_prime_number - integer that print prime number
+ * @n: character
+ * Return: n
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+		return (_helper(n, n - 1));
+}
