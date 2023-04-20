bc552b7f4e9193b1ca95f409c4b8b7ae1852e482
NSHUTI Henry <henrixoson@gmail.com>
2023-04-19 19:38:48 -0700

    C - Function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
@@ -7,57 +6,52 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_add - Returns the sum of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The sum of a and b.
+ * op_add - function
+ * @a: The first
+ * @b: The second
+ * Return: The sum
  */
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
 /**
- * op_sub - Returns the difference of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The difference of a and b.
+ * op_sub - function
+ * @a: The first
+ * @b: The second
+ * Return: The difference
  */
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
 /**
- * op_mul - Returns the product of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The product of a and b.
+ * op_mul - function
+ * @a: The first
+ * @b: The second
+ * Return: The product
  */
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
 /**
- * op_div - Returns the division of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The quotient of a and b.
+ * op_div - function
+ * @a: The first
+ * @b: The second
+ * Return: The quotient
  */
 int op_div(int a, int b)
 {
-	return (a / b);
+return (a / b);
 }
 /**
- * op_mod - Returns the remainder of the division of two numbers.
- * @a: The first number.
- * @b: The second number.
- *
- * Return: The remainder of the division of a by b.
+ * op_mod - function
+ * @a: The first
+ * @b: The second
+ * Return: The remainder
  */
 int op_mod(int a, int b)
 {
-	return (a % b);
+return (a % b);
 }
