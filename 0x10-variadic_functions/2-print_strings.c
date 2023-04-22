e028c08150ccc29d6b78cee4991f62e80147a109
NSHUTI Henry <henrixoson@gmail.com>
2023-04-21 18:07:25 -0700

    The Variadic Functions 3

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,13 +19,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(list, char *);
 		if (!str)
-			str = "null";
+			str = "nil";
 		if (!separator)
 			printf("%s", str);
 		else if (separator && i == 0)
 			printf("%s", str);
 		else
-			printf("%s%s", separator, str);
+			printf("%s%d", separator, str);
 	}
 	printf("\n");
 	va_end(list);
